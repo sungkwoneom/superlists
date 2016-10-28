029eef4a6825aee59585d5b783fc1a49ca40d177
sungkwon.eom <skeom21@gmail.com>
2016-10-28 11:50:20 +0900

    ttt

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,14 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+import re
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
 
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve('/')
@@ -14,7 +20,7 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -22,3 +28,8 @@ class HomePageTest(TestCase):
         request.POST['item_text']='신규 작업 아이템'
         response = home_page(request)
         self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text':'신규 작업 아이템'}
+        )
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
