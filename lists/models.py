abe15d5a705355a6edaf4f4d7aa9f9b48e797b60
sungkwon.eom <skeom21@gmail.com>
2016-11-03 17:53:19 +0900

    done

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+    #text = models.TextField(default='')
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
