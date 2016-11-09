ea2ee24b9bb7821badacbad0eac9b90ff511e361
sungkwon.eom <skeom21@gmail.com>
2016-11-09 18:24:29 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
